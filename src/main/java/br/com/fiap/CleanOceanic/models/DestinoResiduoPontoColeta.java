9a23c9323fb72b5b4d481921a08f08a1c5ebc0ff
RodrigoBatis <rodrigofreirebatista@gmail.com>
2024-06-02 21:18:53 -0300

    Feat: Adiconado Entidade DestinoResiduoPontoColetas

---
+++ src/main/java/br/com/fiap/CleanOceanic/models/DestinoResiduoPontoColeta.java
@@ -0,0 +1,38 @@
+package br.com.fiap.CleanOceanic.models;
+
+import br.com.fiap.CleanOceanic.controllers.dtos.destinoResiduoPontoColeta.DestinoResiduoPontoColetaRegisterDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "T_DESTINO_RESIDUO_PONTO_COLETA")
+public class DestinoResiduoPontoColeta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_destino_residuo_ponto_coleta")
+    private Long idDestinoResiduoPontoColeta;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_destino_residuo")
+    private DestinoResiduo destinoResiduo;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_ponto_coleta")
+    private PontoColeta pontoColeta;
+
+    @Column(name = "dt_created_at")
+    private LocalDateTime createdAt;
+
+    public DestinoResiduoPontoColeta(DestinoResiduoPontoColetaRegisterDTO destinoResiduoPontoColetaRegisterDTO) {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
